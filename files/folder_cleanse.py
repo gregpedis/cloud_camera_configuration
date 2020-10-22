5dec49fcf9cb75293dcc7e24f7d8307a1ce0183d
gregpedis <trafalgar1618@hotmail.com>
2020-10-22 12:18:15 +0300

    Changed the folder_cleanse script

--- files/folder_cleanse.py
+++ files/folder_cleanse.py
@@ -1,15 +1,31 @@
 import os
-import time
+import datetime as dt
 
-BACKUP_INTERVAL = 14400
-THRESHOLD = time.time() - BACKUP_INTERVAL
-
+DAYS_BACK = 5
 FTP_FOLDER = '/home/camera/ftp/files/'
-os.chdir(FTP_FOLDER)
 
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-old_files = [f for f in files if os.path.getctime(f) < THRESHOLD]
 
-for f in old_files:
+def is_old(f):
+    ctime = os.path.getctime(f)
+    mtime = os.path.getmtime(f)  # OS differences are tricky.
+
+    creation_date = dt.date.fromtimestamp(min(ctime, mtime))
+    delta = dt.date.today() - creation_date
+    return delta.days >= DAYS_BACK
+
+
+def remove_files(files):
+    for f in files:
         os.remove(f)
-        
+
+
+def main():
+    os.chdir(FTP_FOLDER)
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+
+    old_files = [f for f in files if is_old(f)]
+    remove_files(old_files)
+
+
+if __name__ == "__main__":
+    main()
